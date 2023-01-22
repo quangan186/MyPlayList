38ffcd6c66cbd9af3761e4d867352ef53f5562ec
quangan186 <61352012+quangan186@users.noreply.github.com>
2023-01-22 17:37:25 +0700

    play audio

--- components/audio/AudioButtonBar.tsx
+++ components/audio/AudioButtonBar.tsx
@@ -4,14 +4,16 @@ import React from "react";
 import play from "@/assets/svg/play.svg";
 import next from "@/assets/svg/next.svg";
 import previous from "@/assets/svg/previous.svg";
+import pause from '@/assets/svg/pause.svg'
 
 interface AudioButtonModel{
   prevFunc?: React.MouseEventHandler<HTMLButtonElement>,
   playFunc?: React.MouseEventHandler<HTMLButtonElement>,
   nextFunc?: React.MouseEventHandler<HTMLButtonElement>,
+  togglePlayButton: boolean
 }
 
-const AudioButtonBar: React.FC<AudioButtonModel> = ({prevFunc, playFunc, nextFunc}) => {
+const AudioButtonBar: React.FC<AudioButtonModel> = ({prevFunc, playFunc, nextFunc, togglePlayButton}) => {
   return (
     <div className="flex justify-center">
       <div className="flex gap-4 items-center">
@@ -24,7 +26,7 @@ const AudioButtonBar: React.FC<AudioButtonModel> = ({prevFunc, playFunc, nextFun
         </Button>
 
         <Button className="flex items-center" onClick={playFunc}>
-          <Image src={play} alt="play" className={`w-[40px] h-[40px]`} />
+          <Image src={togglePlayButton ? pause : play} alt="play" className={`w-[40px] h-[40px]`} />
         </Button>
 
         <Button className="" onClick={nextFunc}>
--- components/audio/MusicBoxContainer.tsx
+++ components/audio/MusicBoxContainer.tsx
@@ -1,21 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import MusicInfo from '../info/MusicInfo'
 import AudioButtonBar from './AudioButtonBar'
 import banner from '@/assets/img/banner.jpg'
 import AudioRange from './AudioProgress'
-const MusicBoxContainer:React.FC = () => {
-  const [togglePlayButton, setTogglePlayButton] = useState(false);
 
+const MusicBoxContainer:React.FC = () => {
+  const [togglePlayButton, setTogglePlayButton] = useState<boolean>(false);
+  const audioRef = useRef<any>();
+  
   const handleOnClickPlayButton = () => {
+    const prevValue = togglePlayButton;
     setTogglePlayButton(state => !state)
+
+    if (!prevValue){
+      audioRef.current.play();
+    } else{
+      audioRef.current.pause();
+    }
   }
 
   return (
-    <div className='bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 px-12 py-8'>
+    <div className='bg-white absolute top-1/2 left-1/2 flex justify-center flex-col items-center -translate-x-1/2 -translate-y-1/2 z-50 px-12 py-8'>
+      <audio ref={audioRef} src="https://youtu.be/sOiMD45QGLs" preload="metadata" ></audio>
       <MusicInfo banner={banner} title='Gira Gira' singer='Ado' />
       <AudioRange start='0:00' end='3:12' className='py-4' />
-      <AudioButtonBar playFunc={handleOnClickPlayButton}/>
-      <span>{togglePlayButton}</span>
+      <AudioButtonBar togglePlayButton={togglePlayButton} playFunc={() => handleOnClickPlayButton()}/>
     </div>
   )
 }

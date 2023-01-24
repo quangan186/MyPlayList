77bbcb4d06852637094a08679cc3dd8cafc7e312
diablo <quangan186@gmail.com>
2023-01-24 22:46:32 +0700

    fix UI

--- components/search/SearchContainer.tsx
+++ components/search/SearchContainer.tsx
@@ -11,7 +11,7 @@ interface SearchBarModel{
 
 const SearchContainer: React.FC<SearchBarModel> = ({searchResult , onSearchClick, onSongClick, onChange}) => {
   return (
-    <div className='w-full flex flex-col '>
+    <div className='w-full flex flex-col'>
         <SearchBar onClick={onSearchClick} onChange={onChange}  />
         <div className={`bg-black w-full h-full flex flex-col gap-4 p-4 mb-8 ${!searchResult ? "invisible" : ""}`}>
             {searchResult && <SearchResult banner={searchResult.snippet.thumbnails.high.url} songName={searchResult.snippet.title} onClick={onSongClick}/>}
